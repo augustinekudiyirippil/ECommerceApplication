6e5c390b0099974b56fb9b5f7dbd08ba9bd3802a
augustinekudiyirippil <augustinekudiyirippil@gmail.com>
2024-10-16 17:19:50 +0100

    Added product controller and view

---
+++ ECommerceWebApplication/ECommerceWebApplication/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceWebApplication.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceWebApplication.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public readonly ApplicationDbContext Context;
+
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+
+       
+
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+    }
+}
